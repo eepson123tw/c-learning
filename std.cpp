58195ba4812f5fbcb01f938ff1e2638c6d511d27
allen <eepson123@gmail.com>
2024-07-08 00:01:04 +0800

    feat(std library): std::cin and std::cout

---
+++ std.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	char x (5);
+	std::cout << "x is equal to:" << x << std::endl;
+	std::cout << "Hi!!" << std::endl;
+	std::cout << "My name is Alex." << std::endl; // std::endl is used to end the line
+	std::cout << "Hi!!" << "123" << std::endl;
+	int y{};
+	std::cin >> y;
+	std::cout << "y is equal to:" << y << '\n';
+	return 0;
+}
+
